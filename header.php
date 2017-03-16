b37b0a7e770af3ece059e16c4f9ae7daba0307ca
Thomas Wicker <thomas@thomaswicker.com>
2017-03-15 18:41:37 -0600

    adding in PHP

--- header.php
+++ header.php
@@ -13,6 +13,15 @@
 
 		<title><?php wp_title(''); ?></title>
 
+		<meta content='WEST Canada' name='description'>
+		<meta content='IE=edge' http-equiv='X-UA-Compatible'>
+		<meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+		<link href='img/favicon.ico' rel='shortcut icon'>
+		<!-- <link href='css/foundation.min.css' rel='stylesheet'> -->
+		<!-- <link href='bower_components/motion-ui/dist/motion-ui.min.css' rel='stylesheet'> -->
+		<!-- <link href='bower_components/components-font-awesome/css/font-awesome.min.css' rel='stylesheet'> -->
+		<!-- <link href='css/application.min.css' rel='stylesheet'> -->
+
 		<?php // mobile meta (hooray!) ?>
 		<meta name="HandheldFriendly" content="True">
 		<meta name="MobileOptimized" content="320">
@@ -24,10 +33,14 @@
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<![endif]-->
+
 		<?php // or, set /favicon.ico for IE10 win ?>
+
 		<meta name="msapplication-TileColor" content="#f01d4f">
+
 		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-            <meta name="theme-color" content="#121212">
+
+		<meta name="theme-color" content="#121212">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
@@ -42,36 +55,75 @@
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
-		<div id="container">
+		<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+			<div class="site-branding">
+				<?php
+				if ( is_front_page() && is_home() ) : ?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php else : ?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+				endif;
 
-				<div id="inner-header" class="wrap cf">
+				$description = get_bloginfo( 'description', 'display' );
+				if ( $description || is_customize_preview() ) : ?>
+					<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+				<?php
+				endif; ?>
+			</div><!-- .site-branding -->
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+			<?php // if you'd like to use the site description you can un-comment it below ?>
+			<?php // bloginfo('description'); ?>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'wca' ); ?></button>
+				<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
+			</nav><!-- #site-navigation -->
 
+			</nav> -->
 
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-    					         'container' => false,                           // remove nav container
-    					         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-    					         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-    					         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-    					         'theme_location' => 'main-nav',                 // where it's located in the theme
-    					         'before' => '',                                 // before the menu
-        			               'after' => '',                                  // after the menu
-        			               'link_before' => '',                            // before each link
-        			               'link_after' => '',                             // after each link
-        			               'depth' => 0,                                   // limit the depth of the nav
-    					         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
+			<a class='btn contact-btn-callout hide-for-small-only' href='contact.html'>
+				<i class='fa fa-phone'></i>
+				Contact Us
+			</a>
 
-					</nav>
+		</header>
 
-				</div>
-
-			</header>
+		<section aria-label='Favorite Space Pictures' class='orbit superhero' data-options='animInFromLeft:fade-in; animInFromRight:fade-in; animOutToLeft:fade-out; animOutToRight:fade-out;' data-orbit='' role='region'>
+			<ul class='orbit-container'>
+				<button class='orbit-previous'>
+					<span class='show-for-sr'>Previous Slide</span>
+					<i class='fa fa-arrow-left'></i>
+				</button>
+				<button class='orbit-next'>
+					<span class='show-for-sr'>Next Slide</span>
+					<i class='fa fa-arrow-right'></i>
+				</button>
+				<li class='is-active orbit-slide'>
+					<strong>WELCOME TO WEST</strong>
+					<figcaption>Avian Species Research and Management</figcaption>
+					<img alt='Eagle' class='orbit-image' src="<?php bloginfo( 'template_directory' ); ?>/img/superhero_img_eagle.jpg">
+				</li>
+				<li class='orbit-slide'>
+					<strong>WELCOME TO WEST</strong>
+					<figcaption>Big Game Research and Monitoring</figcaption>
+					<img alt='Eagle' class='orbit-image' src="<?php bloginfo( 'template_directory' ); ?>/img/superhero_img_bear_on_tree.jpg">
+				</li>
+				<li class='orbit-slide'>
+					<strong>WELCOME TO WEST</strong>
+					<figcaption class='short-title'>Renewable Energy</figcaption>
+					<img alt='Power Lines' class='orbit-image' src="<?php bloginfo( 'template_directory' ); ?>/img/superhero_img_powerlines.jpg">
+				</li>
+				<li class='orbit-slide'>
+					<strong>WELCOME TO WEST</strong>
+					<figcaption class='short-title'>Statistics and Biometrics</figcaption>
+					<img alt='Stats' class='orbit-image' src="<?php bloginfo( 'template_directory' ); ?>/img/superhero_img_stats.jpg">
+				</li>
+				<li class='orbit-slide'>
+					<strong>WELCOME TO WEST</strong>
+					<figcaption class='short-title'>Utilities</figcaption>
+					<img alt='Power Lines' class='orbit-image' src="<?php bloginfo( 'template_directory' ); ?>/img/superhero_img_powerlines.jpg">
+				</li>
+			</ul>
+		</section>
--- index.php
+++ index.php
@@ -1,76 +1,107 @@
 <?php get_header(); ?>
 
-			<div id="content">
+<ul class='sm-nav'>
+  <li>
+    <a href='index.html'>
+      <i class='fa fa-facebook'></i>
+    </a>
+  </li>
+  <li>
+    <a href='index.html'>
+      <i class='fa fa-twitter'></i>
+    </a>
+  </li>
+  <li>
+    <a href='index.html'>
+      <i class='fa fa-linkedin'></i>
+    </a>
+  </li>
+  <li>
+    <a href='index.html'>
+      <i class='fa fa-flickr'></i>
+    </a>
+  </li>
+</ul>
 
-				<div id="inner-content" class="wrap cf">
+<section class='body-content-home'>
 
-						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-
-								<header class="article-header">
-
-									<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-									<p class="byline entry-meta vcard">
-                                                                        <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       								/* the time the post was published */
-                       								'<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       								/* the author of the post */
-                       								'<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    							); ?>
-									</p>
-
-								</header>
-
-								<section class="entry-content cf">
-									<?php the_content(); ?>
-								</section>
-
-								<footer class="article-footer cf">
-									<p class="footer-comment-count">
-										<?php comments_number( __( '<span>No</span> Comments', 'bonestheme' ), __( '<span>One</span> Comment', 'bonestheme' ), __( '<span>%</span> Comments', 'bonestheme' ) );?>
-									</p>
-
-
-                 	<?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-
-								</footer>
-
-							</article>
-
-							<?php endwhile; ?>
-
-									<?php bones_page_navi(); ?>
-
-							<?php else : ?>
-
-									<article id="post-not-found" class="hentry cf">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
-
-							<?php endif; ?>
+  <div class='bch-left'>
+    <h1>Western Ecosystems Technology, ULC</h1>
+    <h2>Providing Scientific Solutions to Natural Resource Challenges in Canada</h2>
+    <p class='body-text'>
+      WEST provides environmental and statistical consulting services and contract research
+      throughout Canada and internationally to industry, government, and private organizations.
+      We offer clients a unique combination of field ecology and statistics to help solve on-going
+      and contemporary natural resource problems.
+    </p>
+    <p class='body-text'>
+      Through the use of state-of-the-art statistical principles in the design, conduct and analysis
+      of ecological field studies, WEST specializes in a common sense, defensible, and professional approach to the solution of natural resource problems. WEST’s expert ecologists and biometricians have broad experience in basic and applied ecological studies and the sophisticated
+      analysis of natural resource data.
+    </p>
 
 
-						</main>
 
-					<?php get_sidebar(); ?>
 
-				</div>
 
-			</div>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+			<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
+
+				<header class="article-header">
+
+					<h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
+
+				</header>
+
+				<section class="entry-content cf">
+					<?php the_content(); ?>
+				</section>
+
+			</article>
+
+		<?php endwhile; ?>
+
+
+
+
+  </div>
+
+
+  <div class='bch-right'>
+    <div class='contact-callout'>
+      <h2>Contact Us</h2>
+      <p class='body-text'>
+        WEST is committed to providing the highest level of professional consulting services resulting in original solutions to a wide range of natural resource problems.
+      </p>
+      <strong>
+        Contact us with your project inquiries.
+      </strong>
+      <div class='btn btn-primary'>
+        Contact
+      </div>
+    </div>
+  </div>
+
+</section>
+
+
+<?php else : ?>
+
+	<article id="post-not-found" class="hentry cf">
+			<header class="article-header">
+				<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+		</header>
+			<section class="entry-content">
+				<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+		</section>
+		<footer class="article-footer">
+				<p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
+		</footer>
+	</article>
+
+<?php endif; ?>
+
+<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
